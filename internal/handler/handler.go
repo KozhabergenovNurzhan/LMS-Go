61ea35fffcbf9bdd0b2694b5ac426354a0f3ba6a
KozhabergenovNurzhan <kozhabergenov24@gmail.com>
2026-02-10 21:03:44 +0500

    getByID Delete Create

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
+	"github.com/KozhabergenovNurzhan/GoProj1/internal/models"
 	"github.com/KozhabergenovNurzhan/GoProj1/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +19,9 @@ func (h *Handler) InitRoutes() (*gin.Engine, error) {
 	r := gin.New()
 
 	r.GET("/courses", h.GetCourses)
-	// TODO implement other endpoints
+	r.GET("/courses/:id", h.GetCourseById)
+	r.DELETE("/courses/:id", h.DeleteCourse)
+	r.POST("/courses", h.CreateCourse)
 
 	return r, nil
 }
@@ -25,6 +30,28 @@ func NewHandler(cs *service.CourseService) *Handler {
 	return &Handler{
 		courseService: cs,
 	}
+}
+
+func (h *Handler) DeleteCourse(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid course id"})
+		return
+	}
+
+	err = h.courseService.DeleteByID(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "course to delete not found"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusNoContent, gin.H{
+		"message": "course is successfully deleted",
+	})
 }
 
 func (h *Handler) GetCourses(c *gin.Context) {
@@ -37,4 +64,45 @@ func (h *Handler) GetCourses(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, courses)
+}
+
+func (h *Handler) GetCourseById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid course id"})
+		return
+	}
+
+	course, err := h.courseService.GetByID(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Course not found"})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, course)
+}
+
+func (h *Handler) CreateCourse(c *gin.Context) {
+	var input models.CreateCourse
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid request body",
+		})
+		return
+	}
+
+	id, err := h.courseService.Create(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to create",
+		})
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"id": id})
 }
--- internal/service/course.go
+++ internal/service/course.go
@@ -16,3 +16,15 @@ func NewCourseService(repo repository.CourseRepo) *CourseService {
 func (cs *CourseService) GetAll() ([]models.Course, error) {
 	return cs.repo.GetAll()
 }
+
+func (cs *CourseService) GetByID(id int) (models.Course, error) {
+	return cs.repo.GetByID(id)
+}
+
+func (cs *CourseService) DeleteByID(id int) error {
+	return cs.repo.DeleteByID(id)
+}
+
+func (cs *CourseService) Create(input models.CreateCourse) (int, error) {
+	return cs.repo.Create(input)
+}
